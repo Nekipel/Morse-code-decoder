a7990a2803d3df53b9a33015042b1975615c72e9
Nekipel <88710417+Nekipel@users.noreply.github.com>
2022-01-30 11:00:06 +0300

    feat: add function decode

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const MORSE_TABLE = {
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
+};
+
+function decode(expr) {
+    let arr = [];
+    for (let i = 0; i <= expr.length-1; i += 10){
+        let tenNumbers = expr.substring(i,i+10);
+        if (tenNumbers.includes("*")){
+            arr.push(" ")
+        }
+        tenNumbers = tenNumbers.replace(/11/g, "-")
+        tenNumbers = tenNumbers.replace(/10/g, ".")
+        tenNumbers = tenNumbers.replace(/00/g, "")
+        arr.push(MORSE_TABLE[tenNumbers]);
+    }
+    return arr.join ('')
+}
+
+module.exports = {
+    decode
+}
